4d7cdd9cc659e7dc2a097cad95ffcd49983fbc06
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-11-07 16:40:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import *
+from numpy import *
+from scipy import *
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import scipy.fftpack as fftp
+
+
+def main():
+    print("main")
