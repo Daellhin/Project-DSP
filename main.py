3d102c8e214d6e8d8692c8e52e369b1617adc5d8
TeunTRBL <70654828+TeunTRBL@users.noreply.github.com>
2023-11-24 11:42:03 +0100

    ADPD op een veel beter pad!:)

--- main.py
+++ main.py
@@ -18,9 +18,23 @@ def channel2APDP(original_data):
     data = transpose(original_data, (1, 2, 0))
     data = reshape(data, (25, 100, 200))
 
-    ifft_amplitude = [abs(fftp.ifft(arr)) for arr in data[:, :]]        #Hier moeten we ook nog eens een venster rond proberen zetten
-    power = [amplitude*amplitude for amplitude in ifft_amplitude[:][:]]
-    avg_power = [mean(power_values) for power_values in power]          #len25 array:)
+    ifft_amplitude = [[abs(fftp.ifft(meas)) for meas in arr] for arr in data]        #Hier moeten we ook nog eens een venster rond proberen zetten
+
+    ifft_amplitude = transpose(ifft_amplitude, (0, 2, 1))
+    ifft_amplitude = reshape(ifft_amplitude, (25, 200, 100))
+
+    power = [
+        [
+            [measurement * measurement for measurement in freq_tonen]
+            for freq_tonen in positions
+        ]
+        for positions in ifft_amplitude
+    ]
+
+    avg_power = [[mean(power_values) for power_values in pos] for pos in power]          #len25 array:)
+
+    plt.plot(avg_power[4])
+    plt.show()
 
     return avg_power
 
@@ -30,6 +44,7 @@ def channel2APDP(original_data):
     # range van de nu bekomen samples in het tijdsdomein (of hier delaydomein), welke delay
     # stelt elk sample voor (wat is de tijdsresolutie) en hoe linkt dit aan de afstand tussen de
     # opeenvolgende frequentietonen en aan de bandbreedte van het signaal?
+    #van 10Mhz gedoe terugwerken naar Ts ; omgekeerde van in labos
 
 
 
@@ -55,7 +70,7 @@ def main():
 
     apdp = channel2APDP(original_data)
 
-    # print(apdp)
+    print(apdp)
 
     plt.plot(apdp)
     plt.show()
